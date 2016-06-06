bbed0db795f79f6a94d34df39de26587a5a8158e
Kan Chen <kan@drchrono.com>
2016-06-06 13:14:22 -0700

    init project

---
+++ DRCRegistrationKit/DRCRegistrationKit/DRCRegistrationKit.h
@@ -0,0 +1,19 @@
+//
+//  DRCRegistrationKit.h
+//  DRCRegistrationKit
+//
+//  Created by Kan Chen on 6/6/16.
+//  Copyright © 2016 drchrono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DRCRegistrationKit.
+FOUNDATION_EXPORT double DRCRegistrationKitVersionNumber;
+
+//! Project version string for DRCRegistrationKit.
+FOUNDATION_EXPORT const unsigned char DRCRegistrationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DRCRegistrationKit/PublicHeader.h>
+
+

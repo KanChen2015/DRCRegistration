042395940684cf03efd37c398f78d65aa73063ee
Kan Chen <kan@drchrono.com>
2016-06-20 10:49:33 -0700

    add OnPatient SignUp

---
+++ DRCRegistrationKit/DRCRegistrationKit/OnPatientLinkViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OnPatientLinkViewController.swift
+//  DRCRegistrationKit
+//
+//  Created by Kan Chen on 6/20/16.
+//  Copyright © 2016 drchrono. All rights reserved.
+//
+
+import UIKit
+
+class OnPatientLinkViewController: UIViewController {
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+    @IBAction func cancelButtonClicked(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func downloadOnPatientClicked(sender: AnyObject) {
+        if let url = NSURL(string: "https://itunes.apple.com/us/app/onpatient-personal-health/id577198251?mt=8") {
+            UIApplication.sharedApplication().openURL(url)
+        }
+    }
+}
